84230b313368f83e37754d7fb4803a014fbf247c
Rajesh <rajesh.mr82@gmail.com>
2021-01-27 19:18:00 +0530

    Create Solution.java

---
+++ src/main/java/Solution.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println("Humanoid Hunt!");
+            Tattoo.getPassword();
+
+    }
+
+}
